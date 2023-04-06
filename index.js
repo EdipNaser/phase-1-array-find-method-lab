f3ec3ce8fe5a1fd175bc29669a520d4ba1093d2a
EdipNaser <naseredib@gmail.com>
2023-04-06 15:48:16 +0300

    All Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function superbowlWin(record) {
+    const a= record.find(findsWins)
+    return a===undefined?undefined:a.year
+  }
+  
+  function findsWins(record) {
+      if (record.result === "W") {
+        return record
+      }
+      else {
+        return undefined
+      }
+  }
+  
+  
+  
+  console.log(superbowlWin(record))
